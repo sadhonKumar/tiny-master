d48f0a85f2b32acf8a918cf9359774b557b2906c
sadhon kumar <43463131+sadhonKumar@users.noreply.github.com>
2020-05-24 19:15:02 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$('document').ready(function(){
+ $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:true,
+    smartSpeed:1000,
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+});
